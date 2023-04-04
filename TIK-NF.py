9609db8dc0d6191e04940de5dd4222021e230579
TweakPY <69265406+Filza2@users.noreply.github.com>
2023-04-04 06:15:58 +0300

    Update TIK-NF.py

--- TIK-NF.py
+++ TIK-NF.py
@@ -1,24 +1,55 @@
-try:import re;from colorama import Fore;from requests import get
-except Exception as e:print(f'[!] Download The Missing Module ! , {e}');exit()
-print("""
-████████╗██╗██╗  ██╗     ███╗   ██╗███████╗
-╚══██╔══╝██║██║ ██╔╝     ████╗  ██║██╔════╝
-   ██║   ██║█████╔╝█████╗██╔██╗ ██║█████╗  
-   ██║   ██║██╔═██╗╚════╝██║╚██╗██║██╔══╝  
-   ██║   ██║██║  ██╗     ██║ ╚████║██║     
-   ╚═╝   ╚═╝╚═╝  ╚═╝     ╚═╝  ╚═══╝╚═╝                                                              
-          By @TweakPY - @vv1ck
+from rich.console import Console
+from requests import get
+import re,os
+
+console=Console()
+
+def header():
+    os.system('cls' if os.name=='nt' else 'clear');console.print("""
+████████╗██╗██╗  ██╗████████╗ ██████╗ ██╗  ██╗     ██╗███╗   ██╗███████╗ ██████╗ 
+╚══██╔══╝██║██║ ██╔╝╚══██╔══╝██╔═══██╗██║ ██╔╝     ██║████╗  ██║██╔════╝██╔═══██╗
+   ██║   ██║█████╔╝    ██║   ██║   ██║█████╔╝█████╗██║██╔██╗ ██║█████╗  ██║   ██║
+   ██║   ██║██╔═██╗    ██║   ██║   ██║██╔═██╗╚════╝██║██║╚██╗██║██╔══╝  ██║   ██║
+   ██║   ██║██║  ██╗   ██║   ╚██████╔╝██║  ██╗     ██║██║ ╚████║██║     ╚██████╔╝
+   ╚═╝   ╚═╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝     ╚═╝╚═╝  ╚═══╝╚═╝      ╚═════╝ 
+                                                                                                                                             
+                            By @TweakPY - @vv1ck
 """)
-user=input(f'[{Fore.RED}?{Fore.WHITE}] Enter The user : ');print('\n')
-rdu=get(f"https://tokcount.com/?user={user}",headers={'Host': 'tokcount.com','User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0','Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8','Accept-Language': 'ar,en-US;q=0.7,en;q=0.3','Accept-Encoding': 'gzip, deflate','Upgrade-Insecure-Requests': '1','Sec-Fetch-Dest': 'document','Sec-Fetch-Mode': 'navigate','Sec-Fetch-Site': 'none','Sec-Fetch-User': '?1','Cache-Control': 'max-age=0','Te': 'trailers'})
-if 'undefined' in re.findall('<title>(.*?)</title>',rdu.text)[0]:exit(f'[!] user Not Found ! , {user}')
-elif 'false' in re.findall('"success":(.*?),',rdu.text)[0]:exit(f'[!] user Not Found ! , {user}')
-elif 'true' in re.findall('"success":(.*?),',rdu.text)[0]:
-    print(f'[{Fore.RED}+{Fore.WHITE}] user id : ',re.findall('"userId":"(.*?)",',rdu.text)[0])
-    print(f'[{Fore.RED}+{Fore.WHITE}] verified : ',re.findall('"verified":(.*?),',rdu.text)[0])
-    print(f'[{Fore.RED}+{Fore.WHITE}] username : ',re.findall('"username":"(.*?)",',rdu.text)[0])
-    print(f'[{Fore.RED}+{Fore.WHITE}] Likes : ',re.findall('"likeCount":(.*?),',rdu.text)[0])    
-    print(f'[{Fore.RED}+{Fore.WHITE}] Followers : ',re.findall('"followerCount":(.*?),',rdu.text)[0])
-    print(f'[{Fore.RED}+{Fore.WHITE}] Following : ',re.findall('"followingCount":(.*?),',rdu.text)[0])
-    print(f'[{Fore.RED}+{Fore.WHITE}] Video Count : ',re.findall('"videoCount":(.*?)},',rdu.text)[0])
-else:exit('[!] Error !! ')
+
+def tiktok_info2(user):
+    try:
+        r=get(f"https://tiktok.livecounts.io/user/search/{user}",headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/111.0','Accept': '*/*','Accept-Language': 'ar,en-US;q=0.7,en;q=0.3','Accept-Encoding': 'gzip, deflate','Origin': 'https://livecounts.io'})
+        if '"success":true' in r.text:
+            console.print(f"""- Name : [bold red]{re.findall('"username":"(.*?)",',r.text)[0]}[/bold red]
+- Bio : [bold red]{re.findall('"signature":"(.*?)",',r.text)[0]}[/bold red]
+- userID : [bold red]{re.findall('"userId":"(.*?)",',r.text)[0]}[/bold red]
+- Verified : [bold red]{re.findall('"verified":(.*?),',r.text)[0]}[/bold red]
+- Followers : [bold red]{re.findall('"followers":(.*?),',r.text)[0]}[/bold red]
+- Following : [bold red]{re.findall('"following":(.*?),',r.text)[0]}[/bold red]
+- Likes : [bold red]{re.findall('"likes":(.*?),',r.text)[0]}[/bold red]
+- Video : [bold red]{re.findall('"videos":(.*?)}}',r.text)[0]}[/bold red]
+- Profile Pic URL : [bold red]{re.findall('"avatar":"(.*?)",',r.text)[0]}[/bold red]""")
+        else:console.print(f"- [bold red]Error[/bold red], [bold red]Can't Get {user} info[/bold red] ! ");exit()
+    except Exception as e:console.print(f"- [bold red]Error[/bold red], [bold red]Can't Get {user} info[/bold red] ! ");exit()
+    
+def tiktok_info():
+    try:
+        header()
+        user=input(f'- Enter The user : ').strip();header()
+        r=get(f"https://tokcount.com/?user={user}",headers={'Host': 'tokcount.com','User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/111.0','Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8','Accept-Language': 'ar,en-US;q=0.7,en;q=0.3','Accept-Encoding': 'gzip, deflate','Upgrade-Insecure-Requests': '1','Sec-Fetch-Dest': 'document','Sec-Fetch-Mode': 'navigate','Sec-Fetch-Site': 'none','Sec-Fetch-User': '?1','Te': 'trailers'})
+        if 'undefined' in re.findall('<title>(.*?)</title>',r.text)[0]:tiktok_info2(user)
+        elif 'false' in re.findall('"success":(.*?),',r.text)[0]:tiktok_info2(user)
+        elif 'true' in re.findall('"success":(.*?),',r.text)[0]:
+            console.print(f"""- userID : [bold red]{re.findall('"userId":"(.*?)",',r.text)[0]}[/bold red]
+- Verified : [bold red]{re.findall('"verified":(.*?),',r.text)[0]}[/bold red]
+- Name : [bold red]{re.findall('"username":"(.*?)",',r.text)[0]}[/bold red]
+- Likes : [bold red]{re.findall('"likes":(.*?),',r.text)[0]}[/bold red]    
+- Followers : [bold red]{re.findall('"followers":(.*?),',r.text)[0]}[/bold red]
+- Following : [bold red]{re.findall('"following":(.*?),',r.text)[0]}[/bold red]
+- Video Count : [bold red]{re.findall('"videos":(.*?)}},',r.text)[0]}[/bold red]""")
+        else:tiktok_info2(user)
+    except Exception as e:tiktok_info2(user)
+    
+    
+    
+tiktok_info()
